43d9e9854753ff11058317fb71affdff9174bfa3
Arif Budiman <52444340+afbudiman@users.noreply.github.com>
2022-03-17 16:49:15 +0700

    edit structure file

--- digit_classification.py
+++ digit_classification.py
@@ -5,34 +5,34 @@ from PIL import Image
 from digit_classification_utils import get_prediction, transform_image
 
 
-# def main():
-canvas_result = st_canvas(
-    fill_color="rgba(255, 165, 0, 0.3)", 
-    stroke_width=12,
-    stroke_color="#FFFFFF",
-    background_color="#000000",
-    height=200,
-    width=200,
-    key="main",
-)
+def main():
+    canvas_result = st_canvas(
+        fill_color="rgba(255, 165, 0, 0.3)", 
+        stroke_width=12,
+        stroke_color="#FFFFFF",
+        background_color="#000000",
+        height=200,
+        width=200,
+        key="main",
+    )
 
-if canvas_result.image_data is not None:
-    canvas_img = canvas_result.image_data.astype('uint8')
-    im = Image.fromarray(canvas_img)
-    img = im.resize((28,28))
-    img_rescaling = img.resize((200, 200), resample=PIL.Image.NEAREST)
-    st.write('Input Image')
-    st.image(img_rescaling)
+    if canvas_result.image_data is not None:
+        canvas_img = canvas_result.image_data.astype('uint8')
+        im = Image.fromarray(canvas_img)
+        img = im.resize((28,28))
+        img_rescaling = img.resize((200, 200), resample=PIL.Image.NEAREST)
+        st.write('Input Image')
+        st.image(img_rescaling)
 
-if st.button('Predict'):
-    tensor = transform_image(im)
-    pred = get_prediction(tensor)
-    st.write(f'Result: {pred[0]}')
-    
+    if st.button('Predict'):
+        tensor = transform_image(im)
+        pred = get_prediction(tensor)
+        st.write(f'Result: {pred[0]}')
+        
 
-# if __name__ == "__main__":
-#     st.set_page_config(
-#         page_title="Streamlit Drawable Canvas Demo", page_icon=":pencil2:"
-#     )
-#     st.title("Drawable Canvas Demo")
-#     main()
+if __name__ == "__main__":
+    st.set_page_config(
+        page_title="Streamlit Drawable Canvas Demo", page_icon=":pencil2:"
+    )
+    st.title("Drawable Canvas Demo")
+    main()
